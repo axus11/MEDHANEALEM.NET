78b491d82973fe4a86dab06c8a07fdd21d012df7
axus11 <136099540+axus11@users.noreply.github.com>
2023-06-09 15:58:41 +0100

    Add files via upload

---
+++ ZZZZ/medish.php
@@ -0,0 +1,21 @@
+<?php
+	$firstName = $_POST['firstName'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into medish(firstName, email, password, ) values(?, ?, ?)");
+		$stmt->bind_param("sssi", $firstName,   $email, $password);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
